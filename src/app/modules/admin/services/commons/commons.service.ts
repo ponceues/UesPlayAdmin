ca1e86e4d1abc5fe0e03e068bf4168c9b910f9f4
aponceli <pe12001@ues.edu.sv>
2026-03-25 15:08:21 -0600

    Se comopleta revision de permisos sobre rol de administrador, se configuran los permisos necesarios.

---
+++ src/app/modules/admin/services/commons/commons.service.ts
@@ -0,0 +1,115 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from '../../../../../environments/environment';
+import { Filter } from '@shared/interfaces/filter';
+import { Observable, throwError } from 'rxjs';
+import { Envelop } from '@shared/interfaces/envelop';
+
+import { catchError } from 'rxjs/operators';
+import { Area } from '@admin/interfaces/area';
+import { License } from '@admin/interfaces/license';
+import { Platform } from '@admin/interfaces/platform';
+import { Device } from '@admin/interfaces/device';
+import { Language } from '@admin/interfaces/language';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CommonsService {
+    private http: HttpClient = inject(HttpClient);
+    private uesPlayApi = `${environment.UesPlayApi}/admin`;
+
+    listAreas(filter: Filter): Observable<Envelop<Area>> {
+        let requestUrl = `${this.uesPlayApi}/areas?page=${filter.page}&pageSize=${filter.pageSize}`;
+
+        if (filter.text !== null) {
+            requestUrl = `${requestUrl}&text=${filter.text}`;
+        }
+
+        if (filter.state !== null) {
+            requestUrl = `${requestUrl}&available=${filter.state}`;
+        }
+
+        return this.http.get<Envelop<Area>>(`${requestUrl}`).pipe(
+            catchError((err: HttpErrorResponse) => {
+                return this.handleErrors(err);
+            })
+        );
+    }
+
+    listLicences(filter: Filter): Observable<Envelop<License>> {
+        let requestUrl = `${this.uesPlayApi}/available-licenses?page=${filter.page}&pageSize=${filter.pageSize}`;
+
+        if (filter.text !== null) {
+            requestUrl = `${requestUrl}&text=${filter.text}`;
+        }
+
+        if (filter.state !== null) {
+            requestUrl = `${requestUrl}&available=${filter.state}`;
+        }
+
+        return this.http.get<Envelop<License>>(`${requestUrl}`).pipe(
+            catchError((err: HttpErrorResponse) => {
+                return this.handleErrors(err);
+            })
+        );
+    }
+
+    listPlatforms(filter: Filter): Observable<Envelop<Platform>> {
+        let requestUrl = `${this.uesPlayApi}/available-platforms?page=${filter.page}&pageSize=${filter.pageSize}`;
+
+        if (filter.text !== null) {
+            requestUrl = `${requestUrl}&text=${filter.text}`;
+        }
+
+        if (filter.state !== null) {
+            requestUrl = `${requestUrl}&available=${filter.state}`;
+        }
+
+        return this.http.get<Envelop<Platform>>(`${requestUrl}`).pipe(
+            catchError((err: HttpErrorResponse) => {
+                return this.handleErrors(err);
+            })
+        );
+    }
+
+    listDevices(filter: Filter): Observable<Envelop<Device>> {
+        let requestUrl = `${this.uesPlayApi}/available-devices?page=${filter.page}&pageSize=${filter.pageSize}`;
+
+        if (filter.text !== null) {
+            requestUrl = `${requestUrl}&text=${filter.text}`;
+        }
+
+        if (filter.state !== null) {
+            requestUrl = `${requestUrl}&available=${filter.state}`;
+        }
+
+        return this.http.get<Envelop<Device>>(`${requestUrl}`).pipe(
+            catchError((err: HttpErrorResponse) => {
+                return this.handleErrors(err);
+            })
+        );
+    }
+
+    listLanguages(filter: Filter): Observable<Envelop<Language>> {
+        let requestUrl = `${this.uesPlayApi}/available-languages?page=${filter.page}&pageSize=${filter.pageSize}`;
+
+        if (filter.text !== null) {
+            requestUrl = `${requestUrl}&text=${filter.text}`;
+        }
+
+        if (filter.state !== null) {
+            requestUrl = `${requestUrl}&available=${filter.state}`;
+        }
+
+        return this.http.get<Envelop<Language>>(`${requestUrl}`).pipe(
+            catchError((err: HttpErrorResponse) => {
+                return this.handleErrors(err);
+            })
+        );
+    }
+
+    private handleErrors(error: HttpErrorResponse): Observable<never> {
+        return throwError(() => error.error);
+    }
+}
